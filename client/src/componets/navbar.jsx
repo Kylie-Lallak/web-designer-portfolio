a9d225281cf1748cc16901eeda0f364a7a348983
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-08-09 22:20:47 -0700

    made first versioin of navbar componet

--- client/src/componets/navbar.jsx
+++ client/src/componets/navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from 'react';
+import '../styles/navbar.css'; // Correct relative path from components to styles
+import { Link } from 'react-router-dom';
 
 
 
 
 export default function navbar() {
   return (
-    <div>
-        navbar
-    </div>
- 
+    <nav className='flex justify-between items-center w-[92%] mx-auto'>
+        <h1>
+            Portfolio
+        </h1>
+
+        <div className='md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[9%] md:w-auto w-full flex items-center px-5 '>
+            <ul className='flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8'>
+                <Link to='/'> <li>Home</li> </Link> 
+                <Link to='/about'> <li>About</li> </Link>   
+                <Link to='/skills'> <li>Skills</li> </Link>  
+                <Link to='/portfolio'> <li>Portfolio</li> </Link> 
+                <Link to='/contact'> <li>Contact</li> </Link> 
+            </ul>
+        </div>
+
+    </nav>
+
   )
 }

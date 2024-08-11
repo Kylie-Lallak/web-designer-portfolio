af13433fab3615eb8a639f2586542255ad3d91e5
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-08-10 20:11:16 -0700

    Added home page final design

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import Headshot from '../images/headshot-uiux-2.png';
+import Phone from '../images/iphone.png';
 import '../styles/home.css'; 
 
 export default function home() {
   return (
-    <div id='background'>
-      <div id='header' className='flex flex-col justify-center items-center text-center'>
+    <div id='background' className='flex'>
 
-        <h2 id="header-intro">Hi I'm Jane Doe</h2>
+      <div id='header' className='flex-col  '>
 
+       
+        <h1 id="header-intro">Hi Im Jane Doe,</h1>
         <div id="header-main-text">
-        and I'm a UI/UX Designer
+        and I'm a <span className='font-bold'>UI/UX Designer</span>
         </div>
 
-        <p> I make great and awesome stuff for cool cleintes and i make it well an good and awesome and i do a good job</p>
-        
-        <button> Learn More</button>
+        <p className='max-w-md'> I'm a UI/UX designer passionate about creating intuitive and visually compelling digital experiences. I focus on crafting functional, aesthetically pleasing interfaces that enhance user interactions.</p>
+      
+      <button id='home-button'className='shadow-lg'>Learn More</button>
+
+    
+      
 
       </div>
+      <img id="phone"src={Phone}></img>
+    
+      
+
  
     </div>
   )

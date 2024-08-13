4793e83ee7311943ff12834b7b65589968043d95
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-08-13 14:31:49 -0700

    fixed small errors in about page

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -10,10 +10,9 @@ export default function about() {
 
              <span className='font-bold'> I Create Designs that Capivate and Connect </span>
          </div>
-         <div id='about-header-box' className='bg-white bg-opacity-50 mt-8  mx-auto rounded-md'>
-         <span className='font-semibold mb-3 text-center block'><h1>About Me</h1> </span>
-         As a professional UI/UX developer, I design modern websites that captivate and engage users. I combine aesthetics with functionality and work closely with developers and designers to bring client visions to life, ensuring seamless and impactful user experiences.
-
+         <div id='about-header-box' className='bg-white bg-opacity-50 mt-8  mx-auto rounded-md shadow-md font-light'>
+         <span className='font-semibold mb-3 text-center block '><h1>About Me</h1> </span>
+         As a professional UI/UX developer, I design modern websites that captivate and engage users. I combine aesthetics with functionality and work closely with developers and designers to bring client visions to life.
 
          </div>
 

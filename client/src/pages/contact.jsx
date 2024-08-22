386b9cea04668ab0fb8fed4e952ae3983bae0f11
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-08-21 20:00:04 -0700

    first finsihed contact card done

--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -11,16 +11,16 @@ export default function contact() {
     <div id='contact-header-text'>
 
         <h1 id='contact-h1 'className=' font-semibold text-center text-4xl sm:text-6xl mt-12'>Let's Talk</h1>
-        <p className=' mx-8 mt-4 text-center sm:mx-32 text-gray-600'>If you have a project in mind or are simply interested in finding out more, get in touch and let’s get things moving.</p>
+        <p className=' mx-8 mt-4 text-center sm:mx-32 text-gray-600'>If you have a project in mind, get in touch and let’s get things moving.</p>
 
     </div>
 
-    <div id='contact-card-container'>
-        <div className='contact-card'>
-          <div className='contact-img-container '> <img src={Phone} className='contact-img'/> </div>
-          <h1>Call Me</h1>
-          <p>I'd love to talk—give me a call at</p>
-          <p>(555) 123-4567</p>
+    <div id='contact-card-container' className='flex flex-col justify-center items-center'>
+        <div className='contact-card mt-10'>
+          <div className='contact-img-container'> <img src={Phone} className='contact-img '/> </div>
+          <h1 className='contact-card-h1'>Call Me</h1> 
+          <p className='contact-card-p '>I'd love to talk—give me a call at</p>
+          <p className='contact-card-p2'>(555) 123-4567</p>
 
         </div>
 

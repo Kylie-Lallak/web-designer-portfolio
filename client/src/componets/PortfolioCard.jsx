30043785052ac3aaf4bbfeff303d8d01a7d56af9
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-09-03 16:55:51 -0700

    Created Portfolio word card componet layout

---
+++ client/src/componets/PortfolioCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/portfolioCard.css';
+
+export default function PortfolioCard() {
+  return (
+
+    <div id='card' className=''>
+
+      <img />
+      <h1 className='text-3xl font-semibold'>Company Name </h1>
+
+      <p>  </p>
+
+      <img />
+
+
+    </div>
+
+
+
+  )
+}
--- client/src/pages/portfolio.jsx
+++ client/src/pages/portfolio.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import '../styles/portfolio.css';
+import Card from '../componets/PortfolioCard';
 
 export default function portfolio() {
   return (
     <>
       <div id='port-header'>
-      <h1 className=' font-semibold text-center text-4xl sm:text-6xl pt-10'>See my Work</h1>
-      <p className=' mx-24 mt-4 text-center sm:mx-32 text-gray-600'> Check out my portfolio. If something sparks your interest, let's connect.</p>
+        <h1 className=' font-semibold text-center text-4xl sm:text-6xl pt-10'>See my Work</h1>
+        <p className=' mx-24 mt-4 text-center sm:mx-32 text-gray-600'> Check out my portfolio. If something sparks your interest, let's connect.</p>
+
+
 
 
       </div>
-    
-    
+
+      <Card />
+
+
     </>
 
   )

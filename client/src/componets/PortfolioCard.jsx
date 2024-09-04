410fd9922346164061cc160b026bc4ef2d00c29b
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-09-03 17:31:32 -0700

    added final portfolio card design and animations

--- client/src/componets/PortfolioCard.jsx
+++ client/src/componets/PortfolioCard.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import '../styles/portfolioCard.css';
+import KnoIcon from '../images/knowlexIcon.png';
+import KnoImage from '../images/knowlexImage.png';
 
 export default function PortfolioCard() {
   return (
 
     <div id='card' className=''>
 
-      <img />
-      <h1 className='text-3xl font-semibold'>Company Name </h1>
+      <div id='card-top' className='my-8 mx-8'>
 
-      <p>  </p>
+        <img className='max-w-20' src={KnoIcon} />
+        <h1 className='text-3xl font-semibold'>Company Name </h1>
 
-      <img />
+        <p className='text-slate-500 leading-relaxed mt-5 max-w-lg' > End to end application for Edtech industry. Knowlex is a platform where to exchange knowledge for other knowledge.  </p>
+
+
+      </div>
+      <div id='card-bottom'>
+        <img src={KnoImage} id='card-image' />
+
+      </div>
+
 
 
     </div>
